756ca7316514b43d24e5df45639d9f9aa102bef9
Vinuta Hiremath <vinuta@capsenta.com>
2019-09-24 16:14:49 +0530

    tests modified

--- sample_test.js
+++ sample_test.js
@@ -5,7 +5,11 @@ var sampleApp = require("./sample_app.js");
 
 describe('Sample Test', function() {
 
-    it('add two numbers', function(done){
-    	sampleApp.add(2,8);
+    it('add two numbers', function(){
+    	expect(sampleApp.add(2,8)).to.equal(10);
+    });
+
+    it('add two numbers error', function(){
+    	expect(sampleApp.add(2,8)).to.equal(20);
     });
 });
