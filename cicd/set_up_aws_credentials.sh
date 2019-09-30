85f53d903b7371abaa5692a97da3461d3a72d840
Vinuta Hiremath <vinuta@capsenta.com>
2019-09-30 16:22:15 +0530

    test

--- cicd/set_up_aws_credentials.sh
+++ cicd/set_up_aws_credentials.sh
@@ -4,10 +4,10 @@ set -o errexit -o pipefail
 
 mkdir -p ~/.aws
 
-if [[ -n ${TEST_AWS_ACCESS_KEY_ID	} && -n ${TEST_AWS_SECRET_KEY}  ]]; then
+if [[ -n ${TEST_AWS_ACCESS_KEY_ID} && -n ${TEST_AWS_SECRET_KEY}  ]]; then
     cat <<EOF >> ~/.aws/credentials
 [grafo-naksha/test]
-aws_access_key_id=${TEST_AWS_ACCESS_KEY_ID	}
+aws_access_key_id=${TEST_AWS_ACCESS_KEY_ID}
 aws_secret_access_key=${TEST_AWS_SECRET_KEY}
 EOF
 fi
